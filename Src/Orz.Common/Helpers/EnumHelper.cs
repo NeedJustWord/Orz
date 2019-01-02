6dabf69c5d31a534a2afacc46db07331e03c8a59
NeedJustWord <13718169+NeedJustWord@users.noreply.github.com>
2019-01-02 10:48:32 +0800

    根据DescriptionAttribute说明或枚举名获取枚举

--- Src/Orz.Common/Helpers/EnumHelper.cs
+++ Src/Orz.Common/Helpers/EnumHelper.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel;
+using Orz.Common.Extensions;
 
 namespace Orz.Common.Helpers
 {
@@ -15,6 +17,22 @@ namespace Orz.Common.Helpers
 		public static TEnum[] GetEnumValues<TEnum>() where TEnum : struct
 		{
 			return (TEnum[])Enum.GetValues(typeof(TEnum));
+		}
+
+		/// <summary>
+		/// 根据<see cref="DescriptionAttribute"/>说明或枚举名获取枚举
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="description"></param>
+		/// <returns></returns>
+		public static T GetEnumName<T>(string description)
+		{
+			foreach (var field in typeof(T).GetFields())
+			{
+				var array = field.GetCustomAttributesSafe<DescriptionAttribute>(false);
+				if ((array.Length > 0 && array[0].Description == description) || field.Name == description) return (T)field.GetValue(null);
+			}
+			throw new ArgumentException($"未能找到对应的枚举：{description}", nameof(description));
 		}
 	}
 }
