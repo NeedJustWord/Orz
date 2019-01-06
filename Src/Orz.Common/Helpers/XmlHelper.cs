e95dd784f6a460955b745caeec1c2b01672e2d7d
NeedJustWord <13718169+NeedJustWord@users.noreply.github.com>
2019-01-06 16:30:00 +0800

    Xml辅助类更新

--- Src/Orz.Common/Helpers/XmlHelper.cs
+++ Src/Orz.Common/Helpers/XmlHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Text;
 using System.Xml.Serialization;
 
 namespace Orz.Common.Helpers
@@ -8,17 +9,22 @@ namespace Orz.Common.Helpers
 	/// </summary>
 	public static class XmlHelper
 	{
-		#region 序列化
+		#region 泛型序列化
 		/// <summary>
 		/// 将实例序列化成xml字符串
 		/// </summary>
+		/// <typeparam name="T"></typeparam>
 		/// <param name="obj"></param>
+		/// <param name="encoding"></param>
 		/// <returns></returns>
-		public static string SerializeObject(object obj)
+		public static string SerializeObject<T>(T obj, Encoding encoding = null)
 		{
+			if (encoding == null) encoding = Encoding.UTF8;
+			//todo:使用encoding
+
 			using (StringWriter stringWriter = new StringWriter())
 			{
-				XmlSerializer xmlSerializer = new XmlSerializer(obj.GetType());
+				XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
 				xmlSerializer.Serialize(stringWriter, obj);
 				stringWriter.Flush();
 				return stringWriter.ToString();
@@ -28,14 +34,19 @@ namespace Orz.Common.Helpers
 		/// <summary>
 		/// 将实例序列化成xml文件
 		/// </summary>
+		/// <typeparam name="T"></typeparam>
 		/// <param name="obj"></param>
 		/// <param name="path"></param>
+		/// <param name="encoding"></param>
 		/// <returns></returns>
-		public static bool SerializeObjectToFile(object obj, string path)
+		public static bool SerializeObjectToFile<T>(T obj, string path, Encoding encoding = null)
 		{
+			if (encoding == null) encoding = Encoding.UTF8;
+			//todo:使用encoding
+
 			using (FileStream fileStream = new FileStream(path, FileMode.Create))
 			{
-				XmlSerializer xmlSerializer = new XmlSerializer(obj.GetType());
+				XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
 				xmlSerializer.Serialize(fileStream, obj);
 				fileStream.Flush();
 				return true;
@@ -49,13 +60,17 @@ namespace Orz.Common.Helpers
 		/// </summary>
 		/// <typeparam name="T"></typeparam>
 		/// <param name="xml"></param>
+		/// <param name="encoding"></param>
 		/// <returns></returns>
-		public static T DeserializeObject<T>(string xml)
+		public static T DeserializeObject<T>(string xml, Encoding encoding = null)
 		{
-			using (StreamReader streamReader = new StreamReader(xml))
+			if (encoding == null) encoding = Encoding.UTF8;
+			//todo:使用encoding
+
+			using (StringReader stringReader = new StringReader(xml))
 			{
 				XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
-				return (T)xmlSerializer.Deserialize(streamReader);
+				return (T)xmlSerializer.Deserialize(stringReader);
 			}
 		}
 
@@ -64,11 +79,15 @@ namespace Orz.Common.Helpers
 		/// </summary>
 		/// <typeparam name="T"></typeparam>
 		/// <param name="path"></param>
+		/// <param name="encoding"></param>
 		/// <exception cref="FileNotFoundException">文件不存在</exception>
 		/// <returns></returns>
-		public static T DeserializeObjectFromFile<T>(string path)
+		public static T DeserializeObjectFromFile<T>(string path, Encoding encoding = null)
 		{
 			if (!File.Exists(path)) throw new FileNotFoundException(nameof(path));
+
+			if (encoding == null) encoding = Encoding.UTF8;
+			//todo:使用encoding
 
 			using (FileStream fileStream = new FileStream(path, FileMode.Open))
 			{
